36330688e17cf3a1f01a5124b5fb1f20306bc5c5
miliyonB <miliyonberhanutulu@gmail.com>
2022-09-12 01:20:59 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,7 +2,7 @@
 /**
  *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
  *Return: Always 0 (Success)
- */
+  */
 int main(void)
 {
 int tens;
@@ -22,7 +22,8 @@ putchar(ones);
 putchar(' ');
 putchar(t);
 putchar(o);
-if (!((tens == '9' && ones == '8') && (t == '9' && o == '9')))
+if (!((tens == '9' && ones == '8') &&
+(t == '9' && o == '9')))
 {
 putchar(',');
 putchar(' ');
