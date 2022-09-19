001dee281b98be9ab1d3121832cec2f371d6b52a
miliyonB <miliyonberhanutulu@gmail.com>
2022-09-19 05:08:32 -0700

    SWAP

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,10 +4,20 @@
 * @a: integer to swap
 * @b: integer to swap
 */
-void swap_int(int *a, int *b)
+void swap(int *xp, int *yp)
 {
-int tmp;
-tmp = *a;
-*a = *b;
-*b = tmp;
+int temp = *xp;
+*xp = *yp;
+*yp = temp;
+}
+int main()
+{
+int x, y;
+printf("Enter Value of x ");
+scanf("%d", &x);
+printf("\nEnter Value of y ");
+scanf("%d", &y);
+swap(&x, &y);
+printf("\nAfter Swapping: x = %d, y = %d", x, y);
+return 0;
 }
